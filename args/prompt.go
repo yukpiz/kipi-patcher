fc63a738893a000ca58ef7ef972855fd614702db
yukpiz <yukpiz@gmail.com>
2016-12-29 10:12:52 +0900

    Print help and version.

--- args/prompt.go
+++ args/prompt.go
@@ -4,7 +4,26 @@ import (
 	"fmt"
 )
 
-func (o *Option) OutputHelp() {
-	fmt.Printf(`
-	`)
+var VersionTmpl string = `
+(•ө•)♡  kipi-patcher v1.0.0
+Downloader of the M*binogi patch files.
+
+Usage: patcher --from <FROM_PATCH_NUM> --to <TO_PATCH_NUM>
+`
+
+var HelpTmpl string = `
+ --to <num>		
+ --from <num>	
+ --patch		Get patch infomations.
+ --version		Show command versions.
+ --info			Show md5 file hashes and size.
+ --server		Show patch server status.
+`
+
+func (o *Option) PrintVersion() {
+	fmt.Println(VersionTmpl)
+}
+
+func (o *Option) PrintHelp() {
+	fmt.Println(VersionTmpl, HelpTmpl)
 }
